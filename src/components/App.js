120f7eeefc1ec874dbedea990a73c5e31220de63
Ilya Saenko <mr.journalist@inbox.ru>
2023-06-25 20:56:24 +0300

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -55,7 +55,7 @@ function App() {
       .then((newCard) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
       })
-      .catch((err) => console.log(" Error like card", err));
+      .catch((err) => console.log("Error like card", err));
   }
 
   function handleCardDelete(card) {
